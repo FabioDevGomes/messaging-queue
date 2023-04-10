f06a4bc8d9cfa0a072fdf05a0dbbb10d4d34e56f
Fábio Dev <fabio.dev.gomes@gmail.com>
2023-04-10 20:31:03 -0300

    [QUEUE-v1.0] - adjusts and refactoring

--- src/main/java/br/fabio/rabbit/MessagingRabbitmqApplication.java
+++ src/main/java/br/fabio/rabbit/MessagingRabbitmqApplication.java
@@ -6,32 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MessagingRabbitmqApplication {
 	
-//	@Bean
-//	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter adapter) {
-//		var container = new SimpleMessageListenerContainer();
-//		container.setConnectionFactory(connectionFactory);
-//		container.setQueueNames(queueName);;
-//		container.setMessageListener(adapter);
-//		
-//		return container;
-//	}
-	
-//	@Bean
-//	MessageListenerAdapter listenerAdapter(ReceiverV2 receiver) {
-//		return new MessageListenerAdapter(receiver, "receiveMessage");
-//	}
-	
-//	@Bean
-//	public MessageConverter josnMessageConverter() {
-//		
-//	}
-	
 	public static void main(String[] args) {
 		SpringApplication.run(MessagingRabbitmqApplication.class, args);
 	}
 	
-	
-	
-	
-
 }
--- src/main/java/br/fabio/rabbit/consumer/ReceiverListener.java
+++ src/main/java/br/fabio/rabbit/consumer/ReceiverListener.java
@@ -1,22 +1,21 @@
 package br.fabio.rabbit.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import br.fabio.rabbit.UserMessageConfig;
+import br.fabio.rabbit.producer.SenderMessage;
 
 @Component
 public class ReceiverListener {
-	
-//	@RabbitListener(
-//			bindings = @QueueBinding(value = @Queue(UserMessageConfig.queueName), 
-//			exchange = @Exchange(name = UserMessageConfig.topicExchangeName), 
-//			key = UserMessageConfig.biding))
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReceiverListener.class);
+
 	@RabbitListener(queues = UserMessageConfig.queueName)
 	public void processMessage(Message message, Object object) {
-		System.out.println("::::::::::: >> " + object);
-		
+		LOGGER.info("::::::::::: >> " + object);
 	}
 
 }
